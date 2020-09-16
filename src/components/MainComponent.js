1c913e76984b751a03bdcaa2b70814cba6d6c546
KartikSgh <2019ucp1394@mnit.ac.in>
2020-09-16 18:10:16 +0530

    React Redux Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -19,11 +19,7 @@ const mapStateToProps = state => {
 }
 
 class Main extends Component {
-
-  constructor(props) {
-    super(props);
-   
-  }  
+  
 
   render() {
 
@@ -49,10 +45,10 @@ class Main extends Component {
         <Header />     
         <Switch>
         <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
