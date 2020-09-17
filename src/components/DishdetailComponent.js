144b1acc0ccae82a3278d5ad92d154c53331133a
KartikSgh <2019ucp1394@mnit.ac.in>
2020-09-17 19:07:43 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form'; 
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -107,6 +108,11 @@ render() {
     function RenderDish({dish}) {
         if (dish != null)
             return(
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
                 <Card key={dish.id}>
                     <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
@@ -114,6 +120,7 @@ render() {
                       <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
+                </FadeTransform>
             );
         else
             return(
@@ -127,8 +134,12 @@ render() {
                 return(
                 <div key={singlecomment.id}>
                     <ul className="list-unstyled">
+                    <Stagger in>
+                    <Fade in>
                         <li>{singlecomment.comment}</li>
                         <li>--{singlecomment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(singlecomment.date)))}</li>
+                    </Fade>
+                    </Stagger>
                     </ul>
                 </div>
                 );
